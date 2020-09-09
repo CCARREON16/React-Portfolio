b4691c51e5bb320adfbddbe96f9a101bc0c702af
Chris Carreon <mrwiggles6545@yahoo.com>
2020-09-08 21:49:58 -0500

    added build files

---
+++ build/precache-manifest.058029e5dda3cb6ed63de0624116a88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b4210754ebfb1fd89c95669a1fb027",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ebb4654a262d94147639",
+    "url": "/React-Portfolio/static/css/main.4283dff0.chunk.css"
+  },
+  {
+    "revision": "d7f3db5d54b911d3f75b",
+    "url": "/React-Portfolio/static/js/2.77950bab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.77950bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb4654a262d94147639",
+    "url": "/React-Portfolio/static/js/main.e2bf4193.chunk.js"
+  },
+  {
+    "revision": "0274f599536680666069",
+    "url": "/React-Portfolio/static/js/runtime-main.27e0f504.js"
+  }
+]);
